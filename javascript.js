52cad97e87bdf34a314c8408c28945712ff751e4
Vinodkumar9983 <digitaldost659@gmail.com>
2025-03-29 16:45:34 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// Simple Search functionality example
+document.querySelector("button").addEventListener("click", function() {
+    const searchQuery = document.querySelector("input").value;
+    alert("Searching for: " + searchQuery);
+});
